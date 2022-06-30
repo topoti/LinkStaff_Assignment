e5df4d11c2bf918eb5071b25c4a03e927a74bb67
topoti <ts03351@gmail.com>
2022-06-30 19:24:09 +0600

    first commit

---
+++ php server/server.php
@@ -0,0 +1,32 @@
+<?php
+	header('Access-Control-Allow-Origin: http://localhost:3000/');
+	$firstNumber = $_POST['firstNumber'];
+	$secondNumber = $_POST['secondNumber'];
+	$operation = $_POST['operation'];
+	/*
+	echo ("FirstNumber : $firstNumber");
+	echo ("SecondNumber: $secondNumber");
+	echo ("Operation: $operation");*/
+
+	if($operation == 'addition'){
+        $result = $firstNumber + $secondNumber;
+		$result = number_format($result,2);
+		echo("Result of addition is: $result");
+    }
+    if($operation == 'subtraction'){
+        $result = $firstNumber - $secondNumber;
+        $result = number_format($result,2);
+        echo("Result of Subtraction is: $result");
+    }
+    if($operation == 'multiplication'){
+        $result = $firstNumber * $secondNumber;
+		$result = number_format($result,2);
+        echo("Result of multiplication is: $result"); 
+    }
+    if($operation == 'division'){
+        $result = $firstNumber / $secondNumber;
+		$result = number_format($result,2);
+        echo("Result of division is: $result");
+    }
+    
+?>
